f05eb84ae2ec0d8d0ac77b2b567cd77732118524
enzotorrs <enzotorres969@gmail.com>
2022-09-29 19:07:02 -0300

    feat: allow all urls in acess-control header

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const fs = require("fs")
 
 const app = express()
 
+app.use((_, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.json());
 
 app.post("/generate", async (req, res) => {
