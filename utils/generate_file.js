7ea63651e21bf2a965b4f41977910396be0bcc49
Enzo Torres <enzotorres969@gmail.com>
2023-02-04 11:01:50 -0300

    feat: add feature of erase files after configurable time

--- utils/generate_file.js
+++ utils/generate_file.js
@@ -1,16 +1,21 @@
+const config = require('config')
+
 const util = require('util')
 const exec = util.promisify(require('child_process').exec);
+const { existsSync, mkdirsync, unlinkSync } = require('fs')
 const { dirname } = require('path');
-const { existsSync, mkdirsync} = require('fs')
 
 const appDir = dirname(require.main.filename);
 
 async function generate_file(size, fileName) {
     const generatedFilesFolder = `${appDir}/generated_files`
-    if(!existsSync(generatedFilesFolder)){
+    if (!existsSync(generatedFilesFolder)) {
         mkdirsync(generatedFilesFolder)
     }
     await exec(`dd if=/dev/zero of="${generatedFilesFolder}/${fileName}" bs=1M count=${size}`)
+    setTimeout(() => {
+        unlinkSync(`${generatedFilesFolder}/${fileName}`)
+    }, config.get('eraseFileAfterMiliseconds'))
 }
 
 module.exports = { generate_file }
