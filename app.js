6e93e99639418a4f8327009ddb5aba2f0dfc729a
Enzo Torres <enzotorres969@gmail.com>
2023-02-12 13:30:04 -0300

    feat: add log in validation and socket events

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { Server } = require("socket.io");
 const config = require('config');
 const { getProgressPercent } = require("./utils/generate_file/get_progress_percent");
+const {logger } = require('./utils/logger')
 const morgan = require('morgan')
 
 const io = new Server(config.get('socketServerPort'), {
@@ -31,16 +32,20 @@ if (process.env.NODE_ENV === "development") {
 
 app.post("/generate", async (req, res) => {
     const { size, file_name, socketId } = req.body
+    logger(`new file request received ${size} ${file_name} ${socketId}`)
 
     if (!size) {
+        logger("size not especified in request")
         return res.status(400).json({ error: "unspecified size" })
     }
 
     if (!file_name) {
+        logger("file name not especified in request")
         return res.status(400).json({ error: "unspecified file_name" })
     }
 
     if (!socketId) {
+        logger("socket id not especified in request")
         return res.status(400).json({ error: "unspecified socketId" })
     }
 
@@ -50,19 +55,22 @@ app.post("/generate", async (req, res) => {
             const stringData = data.toString()
             const percent = getProgressPercent(stringData, size)
             io.to(socketId).emit("progress", { "percent": percent})
+            logger(`onProgess event trigged for ${socketId} socket ${percent}%`)
         },
         // on finish
         () => {
             io.to(socketId).emit("finish_process", { "error": false, "url": `/${file_name}` })
+            logger(`onFinish event trigged for ${socketId} socket`)
         },
         // on Error
         () => {
             io.to(socketId).emit("finish_process", { "error": true })
+            logger(`onError event trigged for ${socketId} socket`)
         })
 
     res.send({ message: "File is being generated" })
 })
 
 app.listen(config.get("serverPort"), () => {
-    console.log("server running")
+    console.log(`server running in port: ${config.get("serverPort")}`)
 })
